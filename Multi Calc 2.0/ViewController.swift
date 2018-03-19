c4da55e847ca2c9abd0a331efa3ade0d4c234688
Me <j.sunderland@setonhill.edu>
2018-03-19 19:51:22 -0400

    Initial Commit

---
+++ Multi Calc 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi Calc 2.0
+//
+//  Created by Jerrod on 3/19/18.
+//  Copyright © 2018 Jerrod Sunderland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
